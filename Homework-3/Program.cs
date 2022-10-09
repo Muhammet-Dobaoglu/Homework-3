16ee538255e2addf7659eeb179778438eac8324a
Muhammet Dobaoğlu <muhammetdobaoglu@hotmail.com>
2022-10-09 21:06:52 +0300

    Proje dosyası ekle.

---
+++ Homework-3/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 1; i <= 10; i++)
+{
+    Console.Write(i + ". Sayıyı Giriniz : ");
+    int sayi = int.Parse(Console.ReadLine());
+
+    if (sayi % 2 == 0) 
+        Console.WriteLine(sayi + " Sayısı Çift Sayıdır...");  
+    else
+        Console.WriteLine(sayi + " Sayısı Tek Sayıdır...");
+
+    Console.WriteLine("");
+}
